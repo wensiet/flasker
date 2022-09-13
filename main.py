081facfd4f3476f9bad6fcdccc9cacfbbee32f37
wensiet <wensietyt@gmail.com>
2022-09-14 00:33:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask import request
+from flask import redirect
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(250), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+def hello():
+    return "Hello world!"
+
+
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error while adding article."
+        pass  # TODO обработать пост запрос и добавить хуету в БД
+    else:
+        return render_template("create-article.html")
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detailed(id):
+    article = Article.query.get(id)
+    return render_template('post_detailed.html', article=article)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
